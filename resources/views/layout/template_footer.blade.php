46253e6ef62f03656bb002412020b4d4c0ba98f0
elizabethgyekyewaaaboagye <egyekyewaaaboagye56@gmail.com>
2023-07-03 12:53:23 -1200

    first commit

---
+++ resources/views/layout/template_footer.blade.php
@@ -0,0 +1,5 @@
+;<div class="footer" style="background-color: #11d9e7;">
+	<div class="container">
+		<center><b class="copyright" style="color: white;">&copy; {{ date('Y') }} - My Online Library Management System!...All rights reserved. </b> </center>
+	</div>
+</div>
